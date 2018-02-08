34a68324da23d66382640bccdbbd0030783e7df2
Antoni Virós Martin <ani300@gmail.com>
2018-02-08 00:16:57 -0500

    fixed answer bug

--- src/components/DaphneAnswer.vue
+++ src/components/DaphneAnswer.vue
@@ -2,7 +2,7 @@
     <div class="panel-block functionality content">
         <img src="assets/img/loader.svg" style="margin: auto;" height="64" width="64" v-if="isLoading">
         <template v-else>
-            <component v-if="!emptyResponse" v-bind:is="responseTypes[getResponse['visual_answer_type']]"></component>
+            <component v-if="!emptyResponse" v-bind:is="responseTypes[daphneResponse['visual_answer_type']]"></component>
             <p v-else>The answers from Daphne will be displayed here.</p>
         </template>
     </div>
