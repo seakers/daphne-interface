3e0b97c9ffe49f012828016bd3f8b8c335fcd4dd
Antoni Virós Martin <ani300@gmail.com>
2018-03-30 15:38:52 -0400

    Experiment functionality done

--- src/components/Stage2Modal.vue
+++ src/components/Stage2Modal.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="message-body">
-        <p>That's it for the experiment! Click Finish to go to the end survey.</p>
-        <a class="button" href="https://cornell.qualtrics.com/jfe/form/SV_1Rmb4JZa4BEDX0h" target="_blank" v-on:click="finishExperiment">Finish</a>
+        <p>That's it for the experiment! Click Finish to go to the end survey, which consists of another test and some general questions.</p>
+        <a class="button" href="https://cornell.qualtrics.com/jfe/form/SV_cGbRBDGT6kx3Q1v" target="_blank" v-on:click="finishExperiment">Finish</a>
     </div>
 </template>
 
