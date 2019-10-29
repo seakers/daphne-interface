248727ebeffebafedccba817ee74b6e6467581d5
Gabe Apaza <gapaza@tamu.edu>
2019-10-28 19:06:04 -0500

    merge conflict fix

--- src/store/index.js
+++ src/store/index.js
@@ -7,11 +7,11 @@ import tradespacePlot from './modules/tradespace-plot';
 import functionalityList from './modules/functionality-list';
 import daphne from './modules/daphne';
 import dataMining from './modules/data-mining';
-import teacherAgent from './modules/teacher-agent';
 import filter from './modules/filter';
 import featureApplication from './modules/feature-application';
 import experiment from './modules/experiment';
 import modal from './modules/modal';
+import teacherAgent from './modules/teacher-agent';
 
 import ClimateCentric from '../scripts/climate-centric';
 import SMAP from '../scripts/smap';
@@ -19,7 +19,6 @@ import Decadal2017Aerosols from '../scripts/decadal';
 import DecadalFilter from '../scripts/decadal-filter';
 import EOSSFilter from '../scripts/eoss-filter';
 import {fetchPost} from "../scripts/fetch-helpers";
-import ReconnectingWebSocket from "reconnecting-websocket";
 
 Vue.use(Vuex);
 
@@ -27,17 +26,10 @@ const debug = process.env.NODE_ENV !== 'production';
 
 export default new Vuex.Store({
     state: {
-        websocket: null
     },
     getters: {
-        getWebsocket(state) {
-            return state.websocket;
-        }
     },
     mutations: {
-        setWebsocket(state, websocket) {
-            state.websocket = websocket;
-        },
     },
     actions: {
         async initProblem({ commit, state }) {
@@ -45,28 +37,28 @@ export default new Vuex.Store({
             let problem = null;
             let filter = null;
             switch (state.problem.problemName) {
-            case 'ClimateCentric':
-                problem = ClimateCentric;
-                filter = EOSSFilter;
-                break;
-            case 'SMAP':
-                problem = SMAP;
-                problem.problemName = 'SMAP';
-                filter = EOSSFilter;
-                break;
-            case 'SMAP_JPL1':
-                problem = SMAP;
-                problem.problemName = 'SMAP_JPL1';
-                filter = EOSSFilter;
-                break;
-            case 'SMAP_JPL2':
-                problem = SMAP;
-                problem.problemName = 'SMAP_JPL2';
-                filter = EOSSFilter;
-                break;
-            case 'Decadal2017Aerosols':
-                problem = Decadal2017Aerosols;
-                filter = DecadalFilter;
+                case 'ClimateCentric':
+                    problem = ClimateCentric;
+                    filter = EOSSFilter;
+                    break;
+                case 'SMAP':
+                    problem = SMAP;
+                    problem.problemName = 'SMAP';
+                    filter = EOSSFilter;
+                    break;
+                case 'SMAP_JPL1':
+                    problem = SMAP;
+                    problem.problemName = 'SMAP_JPL1';
+                    filter = EOSSFilter;
+                    break;
+                case 'SMAP_JPL2':
+                    problem = SMAP;
+                    problem.problemName = 'SMAP_JPL2';
+                    filter = EOSSFilter;
+                    break;
+                case 'Decadal2017Aerosols':
+                    problem = Decadal2017Aerosols;
+                    filter = DecadalFilter;
             }
             commit('setProblem', problem);
             if (filter !== null) {
@@ -91,86 +83,61 @@ export default new Vuex.Store({
                 }
             }
         },
-        async startWebsocket({ state, commit, dispatch, getters }) {
-            return new Promise((resolve, reject) => {
-                // Websocket connection
-                let websocket = new ReconnectingWebSocket(WS_URL + 'eoss/ws');
-                let pingIntervalId = null;
+        async onWebsocketsMessage({ commit, state, getters, dispatch }, message) {
+            let received_info = JSON.parse(message.data);
+            console.log(received_info);
+            if (received_info['type'] === 'ga.new_archs') {
+                received_info['archs'].forEach((arch) => {
+                    dispatch('addNewDataFromGA', arch);
+                });
+            }
+            if (received_info['type'] === 'ga.started') {
+                commit('setGaStatus', true);
+            }
+            if (received_info['type'] === 'ga.finished') {
+                commit('setGaStatus', false);
+            }
+            if (received_info['type'] === 'active.notification') {
+                commit('setNotificationTitle', received_info["notification"]["title"]);
+                commit('setNotificationBody', received_info["notification"]["message"]);
+                commit('setNotificationSetting', received_info["notification"]["setting"]);
+                commit('setShowNotification', true);
+            }
+            if (received_info['type'] === 'active.modification') {
+                commit('setNotificationTitle', received_info["notification"]["title"]);
+                commit('setNotificationBody', received_info["notification"]["message"]);
+                commit('setActiveModification', received_info["modification"]);
+                commit('setNotificationSetting', "modification");
+                commit('setShowNotification', true);
+            }
+            if (received_info['type'] === 'active.live_suggestion') {
+                if (received_info['agent'] === 'engineer') {
+                    commit('addSuggestionListType', {
+                        type: 'engineer',
+                        list: received_info['suggestion_list']
+                    });
 
-                websocket.onopen = function () {
-                    console.log('Web Socket Connection Made');
-
-                    // Start ping routine
-                    pingIntervalId = setInterval(() => {
-                        console.log("Ping sent!");
-                        websocket.send(JSON.stringify({'msg_type': 'ping'}));
-                    }, 10000);
-
-                    // Resolve the promise
-                    resolve();
-                };
-                websocket.onclose = (event) => {
-                    console.error("WebSocket error observed:", event);
-                };
-                websocket.onclose = (event) => {
-                    console.log("Websockets closed", event);
-                    clearInterval(pingIntervalId);
-                };
-                websocket.onmessage = function (event) {
-                    let received_info = JSON.parse(event.data);
-                    console.log(received_info);
-                    if (received_info['type'] === 'ga.new_archs') {
-                        received_info['archs'].forEach((arch) => {
-                            dispatch('addNewDataFromGA', arch);
-                        });
-                    }
-                    if (received_info['type'] === 'ga.started') {
-                        commit('setGaStatus', true);
-                    }
-                    if (received_info['type'] === 'ga.finished') {
-                        commit('setGaStatus', false);
-                    }
-                    if (received_info['type'] === 'active.notification') {
-                        commit('setNotificationTitle', received_info["notification"]["title"]);
-                        commit('setNotificationBody', received_info["notification"]["message"]);
-                        commit('setNotificationSetting', received_info["notification"]["setting"]);
-                        commit('setShowNotification', true);
-                    }
-                    if (received_info['type'] === 'active.modification') {
-                        commit('setNotificationTitle', received_info["notification"]["title"]);
-                        commit('setNotificationBody', received_info["notification"]["message"]);
-                        commit('setActiveModification', received_info["modification"]);
-                        commit('setNotificationSetting', "modification");
-                        commit('setShowNotification', true);
-                    }
-                    if (received_info['type'] === 'active.live_suggestion') {
-                        if (received_info['agent'] === 'engineer') {
-                            commit('addSuggestionListType', {
-                                type: 'engineer',
-                                list: received_info['suggestion_list']
-                            });
-
-                        }
-                        if (received_info['agent'] === 'historian') {
-                            commit('addSuggestionListType', {
-                                type: 'historian',
-                                list: received_info['suggestion_list']
-                            });
-                        }
-                        let daphneResponse = {
-                            "visual_answer_type": "list",
-                            "visual_answer": {
-                                "list": getters.getFullSuggestionsList
-                            }
-                        };
-                        commit('setResponse', daphneResponse);
-                    }
-                    if (received_info['type'] === 'ping') {
-                        console.log("Ping back!");
+                }
+                if (received_info['agent'] === 'historian') {
+                    commit('addSuggestionListType', {
+                        type: 'historian',
+                        list: received_info['suggestion_list']
+                    });
+                }
+                let daphneResponse = {
+                    "visual_answer_type": "list",
+                    "visual_answer": {
+                        "list": getters.getFullSuggestionsList
                     }
                 };
-                commit('setWebsocket', websocket);
-            });
+                commit('setResponse', daphneResponse);
+            }
+            if (received_info['type'] === 'active.message') {
+                commit('addDialoguePiece', received_info['message']);
+            }
+            if (received_info['type'] === 'ping') {
+                console.log("Ping back!");
+            }
         },
         async stopBackgroundTasks({ dispatch }) {
             // Stop all background tasks
@@ -230,11 +197,11 @@ export default new Vuex.Store({
         tradespacePlot,
         functionalityList,
         dataMining,
-        teacherAgent,
         filter,
         featureApplication,
         experiment,
-        modal
+        modal,
+        teacherAgent
     },
-    strict: false
+    strict: debug
 });
