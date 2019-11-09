8160930603453e1946c7a5d97923efdf409024b6
Gabe Apaza <gapaza@tamu.edu>
2019-11-09 14:36:14 -0600

    changed webpack.prod for dev server

--- webpack.prod.js
+++ webpack.prod.js
@@ -8,8 +8,8 @@ module.exports = merge(common, {
     mode: 'production',
     plugins: [
         new webpack.DefinePlugin({
-            'API_URL': JSON.stringify('https://selva-research.engr.tamu.edu/api/'),
-            'WS_URL': JSON.stringify('wss://selva-research.engr.tamu.edu/api/')
+            'API_URL': JSON.stringify('http://18.189.44.130/api/'),
+            'WS_URL': JSON.stringify('ws://18.189.44.130/api/')
         })
     ],
 });
