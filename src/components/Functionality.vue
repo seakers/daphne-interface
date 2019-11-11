642faefe5a68de4d32ada97fbe5d861395749d0a
Gabe Apaza <gapaza@tamu.edu>
2019-11-11 14:05:13 -0600

    testing changes

--- src/components/Functionality.vue
+++ src/components/Functionality.vue
@@ -1,83 +1,22 @@
 <template>
-    <div class="column is-full-mobile" :class="sizeClass">
-        <section class="panel func-panel" :class="funcClass" :id="funcIdString">
-            <p class="panel-heading">
-                <span>{{title}}</span>
-                <span style="float:right;">
-                    <a href="#" class="reduce-panel" v-bind:class="{ 'has-text-grey': isMinSize }" v-on:click.prevent="reduce"><span class="icon"><span class="fas fa-window-minimize"></span></span></a>
-                    <a href="#" class="grow-panel" v-bind:class="{ 'has-text-grey': isMaxSize }" v-on:click.prevent="grow"><span class="icon"><span class="fas fa-window-maximize"></span></span></a>
-                    <a href="#" class="close-panel" v-on:click.prevent="remove"><span class="icon"><span class="fas fa-times"></span></span></a>
-                </span>
-            </p>
-            <component :is="chosenFunctionality" :name="name" :size="size" :func-data="funcData" :func-id="funcIdString"></component>
-        </section>
-    </div>
+    <section class="panel func-panel cheatsheet">
+        <p class="panel-heading">
+            <span>Cheatsheet</span>
+            <span style="float:right;">
+                <a href="#" class="reduce-panel has-text-grey"><span class="icon"><span class="fa fa-window-minimize"></span></span></a>
+                <a href="#" class="grow-panel"><span class="icon"><span class="fa fa-window-maximize"></span></span></a>
+                <a href="#" class="close-panel"><span class="icon"><span class="fa fa-times"></span></span></a>
+            </span>
+        </p>
+    </section>
 </template>
 
 <script>
-    import Cheatsheet from './Cheatsheet';
-    import DesignBuilder from './DesignBuilder';
-    import DaphneAnswer from './DaphneAnswer';
-    import DataMining from './DataMining';
-    import EOSSFilter from './EOSSFilter';
-    import FeatureApplication from './FeatureApplication';
-    import TimelinePlot from "./TimelinePlot";
-    let sizeScale = [
-        'half',
-        'full'
-    ];
     export default {
-        name: 'functionality',
-        props: ['name', 'component', 'title', 'funcClass', 'initialSize', 'funcData', 'funcId'],
-        data () {
-            return {
-                size: this.initialSize,
-                chosenFunctionality: this.component
-            }
-        },
-        components: {
-            Cheatsheet,
-            DesignBuilder,
-            DaphneAnswer,
-            DataMining,
-            EOSSFilter,
-            FeatureApplication,
-            TimelinePlot
-        },
-        computed: {
-            sizeClass: function () {
-                return 'is-' + this.size + '-desktop';
-            },
-            isMinSize: function () {
-                return this.size === this.initialSize;
-            },
-            isMaxSize: function () {
-                return this.size === sizeScale[sizeScale.length - 1];
-            },
-            funcIdString() {
-                return "functionality" + this.funcId;
-            }
-        },
-        methods: {
-            remove: function() {
-                this.$store.commit('closeFunctionality', this.$vnode.key);
-                // PubSub.publish(functionality + '_removed', funcId);
-            },
-            reduce: function() {
-                if (!this.isMinSize) {
-                    // Make column smaller
-                    this.size = sizeScale[sizeScale.indexOf(this.size) - 1];
-                }
-            },
-            grow: function() {
-                if (!this.isMaxSize) {
-                    // Make column bigger
-                    this.size = sizeScale[sizeScale.indexOf(this.size) + 1];
-                }
-            }
-        }
+        name: "functionality"
     }
 </script>
 
 <style scoped>
+
 </style>
