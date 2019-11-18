73a3f481e380cca9d2345040b46a5c824451e946
gapaza <gapaza@tamu.edu>
2019-11-17 20:19:37 -0500

    changed ip for larger aws server

--- webpack.prod.js
+++ webpack.prod.js
@@ -8,8 +8,8 @@ module.exports = merge(common, {
     mode: 'production',
     plugins: [
         new webpack.DefinePlugin({
-            'API_URL': JSON.stringify('http://18.189.44.130/api/'),
-            'WS_URL': JSON.stringify('ws://18.189.44.130/api/')
+            'API_URL': JSON.stringify('http://3.134.113.173/api/'),
+            'WS_URL': JSON.stringify('ws://3.134.113.173/api/')
         })
     ],
 });
