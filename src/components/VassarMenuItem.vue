fbb7ce71730ca5176fab814136b633a17cc41b99
gapaza <gapaza@tamu.edu>
2020-03-17 14:54:56 -0500

    vassar menu item component

---
+++ src/components/VassarMenuItem.vue
@@ -0,0 +1,24 @@
+<template>
+    <a href="#" class="item" v-bind:class="{ active: isActive }" v-on:click.prevent="addVassarFunctionality"><span class="icon"><span class="fas" v-bind:class="icon"></span></span><span class="name">{{ text }}</span></a>
+</template>
+
+<script>
+    export default {
+        name: 'main-menu-item',
+        props: ['name', 'text', 'icon'],
+        computed: {
+            isActive() {
+                return this.$store.state.functionalityList.functionalityCount[this.name] > 0;
+            }
+        },
+        methods: {
+            addVassarFunctionality() {
+                this.$store.commit('addVassarFunctionality', { functionality: this.name, funcData: null });
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
