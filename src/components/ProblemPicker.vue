00e729568049538c000e8775b8a9e023a2e04523
gapaza <gapaza@tamu.edu>
2020-03-16 20:12:55 -0500

    added new and edit buttons to problem picker

--- src/components/ProblemPicker.vue
+++ src/components/ProblemPicker.vue
@@ -1,42 +1,59 @@
 <template>
     <div class="problem-picker">
         <form>
-            <div class="field">
-                <label class="label">Problem:</label>
-                <div class="control">
-                    <div class="select">
-                        <select v-model="problemName">
-                            <option v-for="problem in problemList" v-bind:value="problem">{{ problem }}</option>
-                        </select>
+
+            <div class="problem">
+                <div  id="locate" class="card top-card">
+                    <div class="field">
+                        <label class="label">Problem:</label>
+                        <div class="control">
+                            <div class="select">
+                                <select v-model="problemName">
+                                    <option v-for="problem in problemList" v-bind:value="problem">{{ problem }}</option>
+                                </select>
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </div>
-            <div class="field">
-                <label class="label">Dataset:</label>
-                <div class="control">
-                    <div class="select">
-                        <select v-model="datasetInformation">
-                            <option v-for="dataset in datasetList" v-bind:value="{ filename: dataset.value, user: dataset.user }">{{ dataset.name }}</option>
-                        </select>
+                    <div class="field">
+                        <div class="control">
+                            <button class="button is-link" v-on:click.prevent="changeProblem">New</button>
+                            <button class="button is-link" v-on:click.prevent="changeProblem">Edit</button>
+                        </div>
                     </div>
-                </div>
-            </div>
-            <div class="field">
-                <div class="control">
-                    <button class="button is-link" v-on:click.prevent="changeProblem">Load</button>
-                </div>
-            </div>
-            <div class="field" v-if="isLoggedIn">
-                <div class="control">
-                    <button class="button is-link" v-on:click.prevent="openSaveModal">Save</button>
                 </div>
             </div>
 
-            <div class="field" v-if="isLoggedIn">
-                <div class="control">
-                    <a class="button is-link" v-bind:href="downloadUrl">Download</a>
+            <div class="dataset">
+                <div class="card" >
+                    <div class="field">
+                        <label class="label">Dataset:</label>
+                        <div class="control">
+                            <div class="select">
+                                <select v-model="datasetInformation">
+                                    <option v-for="dataset in datasetList" v-bind:value="{ filename: dataset.value, user: dataset.user }">{{ dataset.name }}</option>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="field">
+                        <div class="control">
+                            <button class="button is-link" v-on:click.prevent="changeProblem">Load</button>
+                        </div>
+                    </div>
+                    <div class="field" v-if="isLoggedIn">
+                        <div class="control">
+                            <button class="button is-link" v-on:click.prevent="openSaveModal">Save</button>
+                        </div>
+                    </div>
+                    <div class="field" v-if="isLoggedIn">
+                        <div class="control">
+                            <a class="button is-link" v-bind:href="downloadUrl">Download</a>
+                        </div>
+                    </div>
                 </div>
             </div>
+
+
         </form>
     </div>
 </template>
@@ -119,9 +136,30 @@
     }
 
     .problem-picker .label {
-        color: #F6F7F7;
+        color: black;
         font-size: 16px;
         font-weight: bold;
         width: 100%;
     }
+
+    .problem-picker .card {
+        background-color: whitesmoke;
+        border-radius: 4px;
+        margin-top: 5px;
+        margin-bottom: 5px;
+        padding: 7px;
+        font-size: 16px;
+        font-weight: bold;
+        width: 100%;
+    }
+
+    .problem-picker .problem {
+        margin-bottom: 15px;
+    }
+
+    .problem-picker .dataset {
+        margin-top: 15px;
+    }
+
+
 </style>
