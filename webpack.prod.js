958372b07fe9646fa865012e7cccee5058b7ed3d
gapaza <gapaza@tamu.edu>
2020-04-04 17:48:22 -0500

    updated prod url

--- webpack.prod.js
+++ webpack.prod.js
@@ -8,8 +8,9 @@ module.exports = merge(common, {
     mode: 'production',
     plugins: [
         new webpack.DefinePlugin({
-            'API_URL': JSON.stringify('https://selva-research.engr.tamu.edu/api/'),
-            'WS_URL': JSON.stringify('wss://selva-research.engr.tamu.edu/api/')
+            'API_URL': JSON.stringify('https://dev.selva-research.com/api/'),
+            'WS_URL': JSON.stringify('wss://dev.selva-research.com/api/'),
+            'GRAPH_QL_URL': JSON.stringify('https://dev.selva-research.com/v1/graphql')
         })
     ],
 });
