1de8aedac519824fd584037c5a1ad1791a000600
EC2 Default User <ec2-user@ip-172-31-28-83.us-east-2.compute.internal>
2021-10-28 20:05:47 +0000

    changes for new dev server

--- webpack.prod.js
+++ webpack.prod.js
@@ -8,10 +8,10 @@ module.exports = merge(common, {
     mode: 'production',
     plugins: [
         new webpack.DefinePlugin({
-            'API_URL': JSON.stringify('https://dev.selva-research.com/api/'),
-            'WS_URL': JSON.stringify('wss://dev.selva-research.com/api/'),
-            'GRAPH_QL_URL': JSON.stringify('https://dev.selva-research.com/v1/graphql'),
-            'GRAPH_QL_WS_URL': JSON.stringify('wss://dev.selva-research.com/v1/graphql'),
+            'API_URL': JSON.stringify('https://dev2.selva-research.com/api/'),
+            'WS_URL': JSON.stringify('wss://dev2.selva-research.com/api/'),
+            'GRAPH_QL_URL': JSON.stringify('https://dev2.selva-research.com/v1/graphql'),
+            'GRAPH_QL_WS_URL': JSON.stringify('wss://dev2.selva-research.com/v1/graphql'),
             'PROBLEM__ID': JSON.stringify('3'),
         })
     ],
