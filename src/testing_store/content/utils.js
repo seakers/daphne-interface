5260a4a11e2875b55375cfa3dc25d7b31a6f3dd0
gapaza <gapaza@tamu.edu>
2022-01-19 14:54:18 -0600

    added imports for the rest of the 401 slides

--- src/testing_store/content/utils.js
+++ src/testing_store/content/utils.js
@@ -4,9 +4,43 @@ import Approaches from './AERO401/Slide5.png'
 
 
 
+import AERO401Slide1 from './AERO401/Slide1.png'
+import AERO401Slide2 from './AERO401/Slide2.png'
+import AERO401Slide3 from './AERO401/Slide3.png'
+import AERO401Slide4 from './AERO401/Slide4.png'
+import AERO401Slide5 from './AERO401/Slide5.png'
 import AERO401Slide6 from './AERO401/Slide6.png'
 import AERO401Slide7 from './AERO401/Slide7.png'
+import AERO401Slide8 from './AERO401/Slide8.png'
+import AERO401Slide9 from './AERO401/Slide9.png'
+import AERO401Slide10 from './AERO401/Slide10.png'
+import AERO401Slide11 from './AERO401/Slide11.png'
+import AERO401Slide12 from './AERO401/Slide12.png'
+import AERO401Slide13 from './AERO401/Slide13.png'
+import AERO401Slide14 from './AERO401/Slide14.png'
+import AERO401Slide15 from './AERO401/Slide15.png'
+import AERO401Slide16 from './AERO401/Slide16.png'
+import AERO401Slide17 from './AERO401/Slide17.png'
+import AERO401Slide18 from './AERO401/Slide18.png'
+import AERO401Slide19 from './AERO401/Slide19.png'
+import AERO401Slide20 from './AERO401/Slide20.png'
+import AERO401Slide21 from './AERO401/Slide21.png'
+import AERO401Slide22 from './AERO401/Slide22.png'
+import AERO401Slide23 from './AERO401/Slide23.png'
+import AERO401Slide24 from './AERO401/Slide24.png'
+import AERO401Slide25 from './AERO401/Slide25.png'
+import AERO401Slide26 from './AERO401/Slide26.png'
+import AERO401Slide27 from './AERO401/Slide27.png'
+import AERO401Slide28 from './AERO401/Slide28.png'
+import AERO401Slide29 from './AERO401/Slide29.png'
 import AERO401Slide30 from './AERO401/Slide30.png'
+import AERO401Slide31 from './AERO401/Slide31.png'
+import AERO401Slide32 from './AERO401/Slide32.png'
+import AERO401Slide33 from './AERO401/Slide33.png'
+import AERO401Slide34 from './AERO401/Slide34.png'
+import AERO401Slide35 from './AERO401/Slide35.png'
+import AERO401Slide36 from './AERO401/Slide36.png'
+import AERO401Slide37 from './AERO401/Slide37.png'
 
 
 
@@ -20,14 +54,116 @@ export function get_slide_src(src){
     else if(src === 'Approaches'){
         return Approaches;
     }
+    else if(src === 'AERO401Slide1'){
+        return AERO401Slide1;
+    }
+    else if(src === 'AERO401Slide2'){
+        return AERO401Slide2;
+    }
+    else if(src === 'AERO401Slide3'){
+        return AERO401Slide3;
+    }
+    else if(src === 'AERO401Slide4'){
+        return AERO401Slide4;
+    }
+    else if(src === 'AERO401Slide5'){
+        return AERO401Slide5;
+    }
     else if(src === 'AERO401Slide6'){
         return AERO401Slide6;
     }
     else if(src === 'AERO401Slide7'){
         return AERO401Slide7;
     }
+    else if(src === 'AERO401Slide8'){
+        return AERO401Slide8;
+    }
+    else if(src === 'AERO401Slide9'){
+        return AERO401Slide9;
+    }
+    else if(src === 'AERO401Slide10'){
+        return AERO401Slide10;
+    }
+    else if(src === 'AERO401Slide11'){
+        return AERO401Slide11;
+    }
+    else if(src === 'AERO401Slide12'){
+        return AERO401Slide12;
+    }
+    else if(src === 'AERO401Slide13'){
+        return AERO401Slide13;
+    }
+    else if(src === 'AERO401Slide14'){
+        return AERO401Slide14;
+    }
+    else if(src === 'AERO401Slide15'){
+        return AERO401Slide15;
+    }
+    else if(src === 'AERO401Slide16'){
+        return AERO401Slide16;
+    }
+    else if(src === 'AERO401Slide17'){
+        return AERO401Slide17;
+    }
+    else if(src === 'AERO401Slide18'){
+        return AERO401Slide18;
+    }
+    else if(src === 'AERO401Slide19'){
+        return AERO401Slide19;
+    }
+    else if(src === 'AERO401Slide20'){
+        return AERO401Slide20;
+    }
+    else if(src === 'AERO401Slide21'){
+        return AERO401Slide21;
+    }
+    else if(src === 'AERO401Slide22'){
+        return AERO401Slide22;
+    }
+    else if(src === 'AERO401Slide23'){
+        return AERO401Slide23;
+    }
+    else if(src === 'AERO401Slide24'){
+        return AERO401Slide24;
+    }
+    else if(src === 'AERO401Slide25'){
+        return AERO401Slide25;
+    }
+    else if(src === 'AERO401Slide26'){
+        return AERO401Slide26;
+    }
+    else if(src === 'AERO401Slide27'){
+        return AERO401Slide27;
+    }
+    else if(src === 'AERO401Slide28'){
+        return AERO401Slide28;
+    }
+    else if(src === 'AERO401Slide29'){
+        return AERO401Slide29;
+    }
     else if(src === 'AERO401Slide30'){
         return AERO401Slide30;
+    }
+    else if(src === 'AERO401Slide31'){
+        return AERO401Slide31;
+    }
+    else if(src === 'AERO401Slide32'){
+        return AERO401Slide32;
+    }
+    else if(src === 'AERO401Slide33'){
+        return AERO401Slide33;
+    }
+    else if(src === 'AERO401Slide34'){
+        return AERO401Slide34;
+    }
+    else if(src === 'AERO401Slide35'){
+        return AERO401Slide35;
+    }
+    else if(src === 'AERO401Slide36'){
+        return AERO401Slide36;
+    }
+    else if(src === 'AERO401Slide37'){
+        return AERO401Slide37;
     }
 
 }
