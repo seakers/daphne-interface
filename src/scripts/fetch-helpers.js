ead4ea60b6f501c0bea9996aa07adf644628b831
gapaza <gapaza@tamu.edu>
2022-10-13 13:47:24 -0500

    updates

--- src/scripts/fetch-helpers.js
+++ src/scripts/fetch-helpers.js
@@ -6,12 +6,10 @@ export async function fetchGet(url) {
 
 export async function fetchPost(url, postBody) {
     let csrftoken = Cookies.get('csrftoken');
-    console.log('--> CSRF TOKEN', csrftoken);
     let reqHeaders = new Headers();
     if (csrftoken !== undefined) {
         reqHeaders.append('X-CSRFToken', csrftoken);
     }
-    console.log('--> REQUEST HEADERS', reqHeaders);
     return fetch(
         url,
         {
--- webpack.prod.js
+++ webpack.prod.js
@@ -8,16 +8,10 @@ module.exports = merge(common, {
     mode: 'production',
     plugins: [
         new webpack.DefinePlugin({
-            // 'API_URL': JSON.stringify('http://daphne-dev-load-balancer-1316892040.us-east-2.elb.amazonaws.com:8000/api/'),
-            // 'WS_URL': JSON.stringify('ws://daphne-dev-load-balancer-1316892040.us-east-2.elb.amazonaws.com:8000/api/'),
-            // 'API_URL': JSON.stringify('https://daphne-dev-load-balancer-1316892040.us-east-2.elb.amazonaws.com:443/api/'),
-            // 'WS_URL': JSON.stringify('wss://daphne-dev-load-balancer-1316892040.us-east-2.elb.amazonaws.com:443/api/'),
             // 'API_URL': JSON.stringify('https://daphne-dev-services.selva-research.com:443/api/'), // PROD
             // 'WS_URL': JSON.stringify('wss://daphne-dev-services.selva-research.com:443/api/'),    // PROD
             'API_URL': JSON.stringify('http://daphne-dev-services.selva-research.com:8000/api/'), // PROD 2
             'WS_URL': JSON.stringify('ws://daphne-dev-services.selva-research.com:8000/api/'),    // PROD 2
-            // 'API_URL': JSON.stringify('http://localhost:8000/api/'),
-            // 'WS_URL': JSON.stringify('ws://localhost:8000/api/'),
             'GRAPH_QL_URL': JSON.stringify('http://52.15.172.32:8080/v1/graphql'),
             'GRAPH_QL_WS_URL': JSON.stringify('ws://52.15.172.32:8080/v1/graphql'),
             'GRAPH_QL_WS_URL_SUB': JSON.stringify('ws://52.15.172.32:8080/subscriptions'),
